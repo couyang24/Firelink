09e480c5a69714a01c038bf302360d93c31afe13
Tiffany Song <54128444+tiffanysn@users.noreply.github.com>
2022-04-04 08:42:14 -0400

    :zap: systematically update (#6)
    * :zap: setup firelink fundation
    
    * :art: add codeql
    
    * :art: added logo
    
    * Add .circleci/config.yml (#3)
    
    Co-authored-by: Owen Ouyang <30837746+couyang24@users.noreply.github.com>
    
    * :pencil: fix circleci
    
    * :green_heart: add pytest
    
    * :green_heart: fix codeql (#4)
    
    * :green_heart: fix codeql
    
    * :green_heart: add code pass badge
    
    Co-authored-by: Owen Ouyang <30837746+couyang24@users.noreply.github.com>
    
    Co-authored-by: Chengran Ouyang <chengran.ouyang@equifax.com>
    Co-authored-by: Owen Ouyang <30837746+couyang24@users.noreply.github.com>

---
+++ firelink/pipeline.py
@@ -0,0 +1,19 @@
+import pickle
+from sklearn.pipeline import Pipeline
+
+class FirePipeline(Pipeline):
+    """wrapper of sklearn pipeline"""
+
+    def __init__(self, steps):
+        super().__init__(steps)
+
+    @classmethod
+    def link_fire(cls, file):
+        """load pickled file"""
+        with open(file, "rb") as obj:
+            return pickle.load(obj)
+
+    def save_fire(self, file, file_type="ember"):
+        if file_type == "ember":
+            with open(file, "wb") as obj:
+                pickle.dump(self, obj)
---
+++ firelink/replace.py
@@ -0,0 +1,14 @@
+from firelink.fire import Firstflame
+
+
+class ConditionalReplace(Firstflame):
+    """ conidtional statement """
+
+    def __init__(self, col, val, cond):
+        self.col = col
+        self.val = val
+        self.cond = cond
+
+    def transform(self, X, y=None):
+        """ transform """
+        pass
